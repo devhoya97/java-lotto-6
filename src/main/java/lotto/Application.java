96655261fdf6dcea51bc23a51cc27cda58fa667e
devhoya97 <diajbh123@gmail.com>
2023-12-07 13:54:04 +0900

    feat: 예외 발생 시 재입력 받기를 제외한 나머지 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,30 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
+        PurchaseAmount purchaseAmount = new PurchaseAmount(InputView.getPurchaseAmount());
+        List<Lotto> lottos = buyLottos(purchaseAmount);
+        OutputView.printLottoNumbers(lottos);
+
+        Lotto winningLotto = new Lotto(InputView.getWinningNumbers());
+        WinningChecker winningChecker = new WinningChecker(winningLotto, InputView.getBonusNumber());
+        List<Prize> prizes = winningChecker.calculatePrizes(lottos);
+        double profitRate = ProfitRateCalculator.calculate(prizes, purchaseAmount.getMoney());
+
+        OutputView.printResult(prizes, profitRate);
+    }
+
+    public static List<Lotto> buyLottos(PurchaseAmount purchaseAmount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int count = 0; count < purchaseAmount.calculateAvailableLottoCount(); count++) {
+            lottos.add(new Lotto());
+        }
+
+        return lottos;
     }
 }
--- src/main/java/lotto/ProfitRateCalculator.java
+++ src/main/java/lotto/ProfitRateCalculator.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class ProfitRateCalculator {
     private static final int PERCENTAGE_MULTIPLIER = 100;
 
-    public double calculate(List<Prize> prizes, long money) {
+    public static double calculate(List<Prize> prizes, long money) {
         long totalPrizeMoney = 0;
 
         for (Prize prize : prizes) {
