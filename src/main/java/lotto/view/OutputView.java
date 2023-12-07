4ec66b12ab9d04ee9e70af50f96fb6a8403747e4
devhoya97 <diajbh123@gmail.com>
2023-12-07 13:36:20 +0900

    feat: 당첨 내역과 수익률을 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,17 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lotto.Lotto;
+import lotto.Prize;
 
 public class OutputView {
     private static final String NEW_LINE = "\n";
+    private static final String MONEY_PATTERN = "#,###";
+    private static final double ROUNDING_MULTIPLIER = 100.0;
     public static void printLottoNumbers(List<Lotto> lottos) {
         System.out.println(NEW_LINE + lottos.size() + "개를 구매했습니다.");
 
@@ -16,5 +21,36 @@ public class OutputView {
 
             System.out.println(numbers);
         }
+    }
+
+    public static void printResult(List<Prize> prizes, double profitRate) {
+        StringBuilder message = new StringBuilder();
+
+        message.append("당첨 통계").append(NEW_LINE).append("---").append(NEW_LINE);
+        for (Prize prize : prizes) {
+            message.append(prize.getCorrectCount()).append("개 일치 (")
+                    .append(getFormattedMoney(prize.getWinningMoney())).append("원) - ")
+                    .append(countPrize(prizes, prize)).append("개").append(NEW_LINE);
+        }
+
+        message.append("총 수익률은 ").append(roundToTwoDecimalPlaces(profitRate)).append
+                .append("%입니다.").append(NEW_LINE);
+
+        System.out.println(message);
+    }
+
+    private static int countPrize(List<Prize> prizes, Prize prize) {
+        return Collections.frequency(prizes, prize);
+    }
+
+    private static String getFormattedMoney(long money) {
+        DecimalFormat formatter = new DecimalFormat(MONEY_PATTERN);
+        return formatter.format(money);
+    }
+
+    public static String roundToTwoDecimalPlaces(double profitRate) {
+        double roundedNumber = Math.round(profitRate * ROUNDING_MULTIPLIER) / ROUNDING_MULTIPLIER;
+
+        return String.format("%.2f", roundedNumber);
     }
 }
