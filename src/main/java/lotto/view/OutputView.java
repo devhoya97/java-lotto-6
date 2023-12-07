cdc574093ddd549da547b772986f9946b672b07c
devhoya97 <diajbh123@gmail.com>
2023-12-07 13:15:19 +0900

    feat: 발행한 로또 수량 및 번호를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,13 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.Lotto;
+
 public class OutputView {
+    private static final String NEW_LINE = "\n";
+    public static void printLottoNumbers(List<Lotto> lottos) {
+        System.out.println(NEW_LINE + lottos.size() + "개를 구매했습니다.");
+
+        lottos.forEach(System.out::println);
+    }
 }
