480cf63cdfc4cac6987db3db8c11b558c54ddac7
devhoya97 <diajbh123@gmail.com>
2023-12-07 12:52:03 +0900

    feat: 로또 구입 금액이 로또 가격으로 나누어 떨어지는지 검증하는 기능 구현

--- src/main/java/lotto/PurchaseAmount.java
+++ src/main/java/lotto/PurchaseAmount.java
@@ -1,9 +1,21 @@
 package lotto;
 
+import static lotto.LottoConstants.ERROR;
+
 public class PurchaseAmount {
 
-    private void validate(int money) {
+    private static final long PRICE_PER_LOTTO = 1000;
+    private final long money;
 
+    public PurchaseAmount(long money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(long money) {
+        if (money % PRICE_PER_LOTTO != 0) {
+            throw new IllegalArgumentException(ERROR + "로또 구입 금액이 로또 가격으로 나누어 떨어지지 않습니다.");
+        }
     }
 
     public int calculateAvailableLottoCount() {
